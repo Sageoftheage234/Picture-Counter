6b275f52f1cc1264828a517482883713f82d2665
Sageoftheage234 <Jourdane234@yahoo.com>
2018-09-11 19:40:49 -0400

    Add files via upload
    updated js file

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let characterImageArray = [];
 
 
-let  image = document.getElementById('image');
+let  imageOne = document.getElementById('image1');
+let  imageTwo = document.getElementById('image2');
+let  imageThree = document.getElementById('image3')
 
 
 let CharacterImage  = function(name, show, filePath){
@@ -12,7 +14,7 @@ let CharacterImage  = function(name, show, filePath){
     this.shown = 0;
 };
 //let [variable] = newCharacterImage('Name','Show','filePath');
-let naruto = newCharacterImage('Naruto','Naruto', './Code-Partners-Click-Pic-Project/Naruto/Naruto-Naruto.jpg');
+let naruto = newCharacterImage('Naruto','Naruto','./Code-Partners-Click-Pic-Project/Naruto/Naruto-Naruto.jpg');
 let sasuke = newCharacterImage('Sasuke','Naruto','./Code-Partners-Click-Pic-Project/Naruto/Sasuke-Naruto.jpg');
 let sakura = newCharacterImage('Sakura','Naruto','.Code-Partners-Click-Pic-Project/Naruto/Sakura-Naruto.jpg');
 let itachi = newCharacterImage('Itachi','Naruto','./Code-Partners-Click-Pic-Project/Naruto/Itachi-Naruto.jpg');
@@ -66,28 +68,28 @@ displayImages();
 firstImage = randomImage();
 secondImage = randomImage();
 thirdImage = randomImage();
-fourthImage = randomImage();
-fifthImage = randomImage();
-sixthImage = randomImage();
-seventhImage = randomImage();
-eighthImage = randomImage();
-ninthImage = randomImage();
-tenthImage = randomImage();
-eleventhImage = randomImage();
-twelvthImage = randomImage();
-thirteenthImage = randomImage();
-fourteenthImage = randomImage();
-fifteenthImage = randomImage();
-sixteenthImage = randomImage();
-seventeenthImage = randomImage();
-eighteenthImage = randomImage();
-nineteenthImage = randomImage();
-twentiethImage = randomImage();
-twentyfirstImage = randomImage();
-twentysecondImage = randomImage();
-twentythirdImage = randomImage();
-twentyfourthImage = randomImage();
-twentyfifthImage = randomImage();
+// fourthImage = randomImage();
+// fifthImage = randomImage();
+// sixthImage = randomImage();
+// seventhImage = randomImage();
+// eighthImage = randomImage();
+// ninthImage = randomImage();
+// tenthImage = randomImage();
+// eleventhImage = randomImage();
+// twelvthImage = randomImage();
+// thirteenthImage = randomImage();
+// fourteenthImage = randomImage();
+// fifteenthImage = randomImage();
+// sixteenthImage = randomImage();
+// seventeenthImage = randomImage();
+// eighteenthImage = randomImage();
+// nineteenthImage = randomImage();
+// twentiethImage = randomImage();
+// twentyfirstImage = randomImage();
+// twentysecondImage = randomImage();
+// twentythirdImage = randomImage();
+// twentyfourthImage = randomImage();
+// twentyfifthImage = randomImage();
 
 
 
@@ -97,39 +99,41 @@ twentyfifthImage = randomImage();
 imageOne.src = firstImage.filePath
 imageTwo.src = secondImage.filePath;
 imageThree.src = thirdImage.filePath;
-imageFour.src = fourthImage.filePath;
-imageFive.src = fifthImage.filePath;
-imageSix.src = sixthImage.filePath;
-imageSeven.src = seventhImage.filePath;
-imageEight.src = eighthImage.filePath;
-imageNine.src = ninthImage.filePath;
-imageTen.src = tenthImage.filePath;
-imageEleven.src = eleventhImage.filePath;
-imageTwelve.src = twelvthImage.filePath;
-imageThirtheen.src = thirteenthImage.filePath;
-imageFourteen.src = fourteenthImage.filePath;
-imageFifteen.src = fifteenthImage.filePath;
-imageSixteen.src = sixteenthImage.filePath;
-imageSeventeen.src = seventeenthImage.filePath;
-imageEighteen.src = eighteenthImage.filePath;
-imageNineteen.src = nineteenthImage.filePath;
-imageTwenty.src = twentiethImage.filePath;
-imageTwentyOne.src = twentyfirstImage.filePath;
-imageTwentyTwo.src = twentysecondImage.filePath;
-imageTwentyThree.src = twentythirdImage.filePath;
-imageTwentyFour.src = twentyfourthImage.filePath;
-imageTwentyFive.src = twentyfifthImage.filePath;
+// imageFour.src = fourthImage.filePath;
+// imageFive.src = fifthImage.filePath;
+// imageSix.src = sixthImage.filePath;
+// imageSeven.src = seventhImage.filePath;
+// imageEight.src = eighthImage.filePath;
+// imageNine.src = ninthImage.filePath;
+// imageTen.src = tenthImage.filePath;
+// imageEleven.src = eleventhImage.filePath;
+// imageTwelve.src = twelvthImage.filePath;
+// imageThirtheen.src = thirteenthImage.filePath;
+// imageFourteen.src = fourteenthImage.filePath;
+// imageFifteen.src = fifteenthImage.filePath;
+// imageSixteen.src = sixteenthImage.filePath;
+// imageSeventeen.src = seventeenthImage.filePath;
+// imageEighteen.src = eighteenthImage.filePath;
+// imageNineteen.src = nineteenthImage.filePath;
+// imageTwenty.src = twentiethImage.filePath;
+// imageTwentyOne.src = twentyfirstImage.filePath;
+// imageTwentyTwo.src = twentysecondImage.filePath;
+// imageTwentyThree.src = twentythirdImage.filePath;
+// imageTwentyFour.src = twentyfourthImage.filePath;
+// imageTwentyFive.src = twentyfifthImage.filePath;
 
 
 function imageClicked(event){
-    numberOrderImage.clicked += 1;
+    firstImage.clicked += 1;
     console.log('event', event.target.id)
     console.log('number image clicked property', numberOrderImage.clicked)
 
 }
 
 //add an event listener to each displayed picture to capture click
-imageNum.addEventListener('click', imageClicked);
+imageOne.addEventListener('click', imageClicked);
+imageTwo.addEventListener('click', imageClicked);
+imageThree.addEventListener('click', imageClicked);
 
 
 
